9787b5c4734f5589392cccc35571f2c74d390afb
DarkAdminAccount <54109349+DarkAdminAccount@users.noreply.github.com>
2019-08-14 17:36:46 +0200

    Create tools_installer.sh
    chmod +x *
    ./tools_installer.sh(sh or bash or ./ + tools_installer.sh)

---
+++ tools_installer.sh
@@ -0,0 +1,26 @@
+echo '[+] started installing .....'
+echo '[*] installing the Python3 DEV'
+pkg install python
+echo '[+] installed python3'
+echo ''
+echo '[*] installing the Python2 DEV'
+pkg install python2
+echo 'installed Python2 DEV ....'
+echo ''
+echo '[*] installing the nmap ....'
+pkg install nmap
+echo '[+] installed the nmap scan tool'
+echo ''
+echo '[*] installing the Perl DEV ....'
+pkg install perl
+echo '[+] installed the perl dev'
+echo ''
+echo '[*] installing the ruby DEV ....'
+pkg install ruby
+echo '[+] installed the ruby DEV'
+echo ''
+echo '[*] installing the c++[cpp(g++)] DEV ....'
+apt install g++
+echo '[+] installed  the c++[cpp(g++)] DEV'
+echo ''
+echo ''
